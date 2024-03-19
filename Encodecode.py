28ccbca378b4f5599bcd05ae672b8e58b581040e
Jmcisnerosjr <164077710+Jmcisnerosjr@users.noreply.github.com>
2024-03-19 17:00:55 -0400

    Create Encodecode.py

---
+++ Encodecode.py
@@ -0,0 +1,13 @@
+import cryptography
+
+dic = {"1": 4, "2": 5, "3": 6, "4": 7, "5": 1, "6": 2, "7": 3, "8": 4}
+
+
+print("Menu")
+print("-------------")
+print("1. Encode")
+print("2. Decode")
+print("3. Quit")
+
+
+opt = int(input())
